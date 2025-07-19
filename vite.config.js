60e0fdb2f26ce2fc5988a2e68114a234d482972b
Sabina Gasanova <sabina21gasanova@yandex.ru>
2025-07-19 13:38:34 +0300

    change configs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,74 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import pugPlugin from "vite-plugin-pug";
+
+const root = resolve(__dirname, "./");
+const outDir = resolve(__dirname, "dist");
+
+const options = { pretty: true };
+const locals = { name: "My Pug" };
+
 export default defineConfig({
-  base: "./",
-  esbuild: {
-    supported: {
-      "top-level-await": true,
-    },
+  preview: {
+    port: 3000,
+    strictPort: true,
   },
-  build: {
-    outDir: "./examples",
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
   },
 
-  plugins: [nodePolyfills()],
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+    pugPlugin(options, locals),
+    nodePolyfills(),
+  ],
+
+  root,
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "/src"),
+    },
+  },
+
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        test: resolve(root, "index.html"),
+      },
+      output: {
+        assetFileNames: ({ name }) => {
+          name = name.toLowerCase();
+
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/styles/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
